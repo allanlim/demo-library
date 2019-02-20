7260774e792b40a05aa7cc445afe63a31504c4ce
Allan Lim <allangators@gmail.com>
2019-02-20 13:45:26 -0500

    Update notifySlack.groovy

--- vars/notifySlack.groovy
+++ vars/notifySlack.groovy
@@ -26,7 +26,7 @@ def call(String buildStatus = 'STARTED', String channel = '#engineering') {
 
   def commit = "${env.GIT_COMMIT}"
   def author = "${env.GIT_AUTHOR}"
-  def message = bat(returnStdout: true, script: 'git log --pretty="format: * %h - %b [%an]"').trim()
+  def message = bat(returnStdout: true, script: 'git log --pretty=oneline').trim()
 
   // Override default values based on build status
   if (buildStatus == 'STARTED') {
