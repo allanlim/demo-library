60761cfeef7f6abf02d96415af675b4d7757e4b2
Allan Lim <allangators@gmail.com>
2019-02-21 16:33:58 -0500

    Update notifySlack.groovy

--- vars/notifySlack.groovy
+++ vars/notifySlack.groovy
@@ -120,7 +120,7 @@ def call(String buildStatus = 'STARTED', String channel = '#engineering') {
         for (int j = 0; j < entries.length; j++) {
             def entry = entries[j]
             truncated_msg = entry.msg.take(MAX_MSG_LEN)
-            committer = entry.commit.author.name
+            committer = entry.commitId.author
             commit = entry.commitId.take(7)
             
             // files = entry.file.editType.name
